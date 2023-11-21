ab2331be313ac3f4cafef254757dbeadf8abe811
thugperfect <meenamani9444@gmail.com>
2023-11-21 19:59:08 +0530

    string degree to numeric

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const url = "https://imagetoolkitextension.onrender.com"
+const url = "https://1dc3-2401-4900-632b-f927-ef06-a61c-24db-e2dd.ngrok.io"
 
 module.exports = url
--- imageTools/rotateImage.js
+++ imageTools/rotateImage.js
@@ -2,17 +2,15 @@ const sharp = require('sharp')
 const rotateImage = async (req, name) => {
     try{
     const imageUrl = req.body.params.form?.values?.rotate.files.url;
-    const degree = req.body.params.form?.values?.degree.value;
+    const degree = Number(req.body.params.form?.values?.degree.value);
     const response = await fetch(imageUrl);
 
     const buffer = await response.arrayBuffer();
 
     const resizedImage = sharp(buffer);
-    
 
     await resizedImage.withMetadata().rotate(degree).toFile(`./public/${name}`);
 
-    console.log("Image resizing and saving complete");
   } catch (error) {
       console.log(error)
   }
