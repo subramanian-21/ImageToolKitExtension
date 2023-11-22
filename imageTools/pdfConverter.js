0509037cf2bb7db1aad244390a219ef3e8986e53
thugperfect <meenamani9444@gmail.com>
2023-11-22 19:57:29 +0530

    changed url

--- imageTools/pdfConverter.js
+++ imageTools/pdfConverter.js
@@ -1,33 +1,37 @@
 const { PDFDocument } = require("pdf-lib");
 const fs = require("fs");
 const fetch = require("node-fetch");
-const sharp = require('sharp')
 
 const pdfConverter = async (req, uniqueName) => {
-  const images = req.body?.params?.form?.values?.pdfconverter?.files.map(
-    (k) => {
-      return {
-        
-        url: k.url,
-        format: k.contenttype.split("/")[1],
-      };
+  let images;
+  console.log(req.body?.params?.form?.values?.pdfconverter?.files)
+  if(req.body?.params?.form?.values?.pdfconverter?.files.length>0){
+    images = req.body?.params?.form?.values?.pdfconverter?.files.map(
+      (k) => {
+        return {
+          url: k.url,
+          format: k.contenttype.split("/")[1],
+        };
+      }
+    );
+  }else{
+    images = {
+      url:req.body?.params?.form?.values?.pdfconverter?.files.url,
+      format :req.body?.params?.form?.values?.pdfconverter?.files.contenttype.split("/")[1] 
     }
-  );
+  }
+  
 
   const pdfDoc = await PDFDocument.create();
 
   async function createPdf(k) {
     const page = pdfDoc.addPage();
     const jpgImageBytes = await fetch(k.url).then((res) => res.arrayBuffer());
-    const sharpImage = sharp(jpgImageBytes)
-   
     let jpgImage = null;
     if (k.format === "jpg" || k.format === "jpeg") {
-      const changedImage = sharpImage.jpeg({quality:50}).toBuffer()
-      jpgImage = await pdfDoc.embedJpg(changedImage);
+      jpgImage = await pdfDoc.embedJpg(jpgImageBytes);
     } else {
-      const changedImage = sharpImage.png({quality:50}).toBuffer()
-      jpgImage = await pdfDoc.embedPng(changedImage);
+      jpgImage = await pdfDoc.embedPng(jpgImageBytes);
     }
     const { width, height } = jpgImage.scale(1);
     page.setSize(width, height);
@@ -39,6 +43,7 @@ const pdfConverter = async (req, uniqueName) => {
       height,
     });
   }
+
 
   await Promise.all(images.map(async (k) => await createPdf(k)));
   fs.writeFileSync(`./public/${uniqueName}`, await pdfDoc.save());
