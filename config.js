704559b2a2d2e9a752170a672927a995d66dd00e
thugperfect <meenamani9444@gmail.com>
2023-11-24 12:18:10 +0530

    changed pdf coonvert

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const url = "https://imagetoolkitextension.onrender.com"
+const url = "https://4f33-2401-4900-634a-f733-6b6e-e229-ab7c-77d0.ngrok.io"
 
 module.exports = url
--- imageTools/pdfConverter.js
+++ imageTools/pdfConverter.js
@@ -1,9 +1,9 @@
 const { PDFDocument } = require("pdf-lib");
 const fs = require("fs");
 const fetch = require("node-fetch");
+const sharp = require('sharp');
 
 const pdfConverter = async (req, uniqueName) => {
-  
   const images = req.body?.params?.form?.values?.pdfconverter?.files.map((k) => {
     return {
       url: k.url,
@@ -13,27 +13,34 @@ const pdfConverter = async (req, uniqueName) => {
 
   const pdfDoc = await PDFDocument.create();
 
-  async function createPdf(k) {
-    const page = pdfDoc.addPage();
-    const jpgImageBytes = await fetch(k.url).then((res) => res.arrayBuffer());
-    let jpgImage = null;
-    if (k.format === "jpg" || k.format === "jpeg") {
-      jpgImage = await pdfDoc.embedJpg(jpgImageBytes);
-    } else {
-      jpgImage = await pdfDoc.embedPng(jpgImageBytes);
-    }
-    const { width, height } = jpgImage.scale(1);
-    page.setSize(width, height);
+  const embedImage = async (url, format) => {
+    const imageBuffer = await fetch(url).then((res) => res.buffer());
+    const sharpImage = sharp(imageBuffer).withMetadata();
+    const processedImage = await sharpImage.toBuffer();
 
+    if (format === "jpg" || format === "jpeg") {
+      return pdfDoc.embedJpg(processedImage);
+    } else {
+      return pdfDoc.embedPng(processedImage);
+    }
+  };
+
+  const embedImages = await Promise.all(images.map(({ url, format }) => embedImage(url, format)));
+
+  embedImages.forEach((jpgImage) => {
+    const page = pdfDoc.addPage();
+    const { width, height } = jpgImage.scale(1);
+
+    page.setSize(width, height);
     page.drawImage(jpgImage, {
       x: 0,
       y: 0,
       width,
       height,
     });
-  }
+  });
 
-  await Promise.all(images.map(async (k) => await createPdf(k)));
   fs.writeFileSync(`./public/${uniqueName}`, await pdfDoc.save());
 };
+
 module.exports = pdfConverter;
