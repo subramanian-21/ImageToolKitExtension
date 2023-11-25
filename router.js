c9becfe6ee0bfaea994589230ac0399c8e9a9b2e
thugperfect <meenamani9444@gmail.com>
2023-11-26 00:42:23 +0530

    complete loading

--- router.js
+++ router.js
@@ -7,22 +7,30 @@ router.post("/api", controller);
 router.get("/download/:fileName", (req, res) => {
   const fileName = req.params.fileName;
   const filePath = path.join(__dirname, "public", fileName);
-  res.sendFile(path.join(__dirname, "staticAssets", "index.html"));
-  fs.stat(filePath, (err, stats) => {
-    if (err) {
-      console.error(err);
+//   fs.stat(filePath, (err, stats) => {
+//     if (err) {
+//       console.error(err);
+//     } else {
+//       if (stats.isFile()) {
+//         res.download(filePath, fileName, (err) => {
+//           if (err) {
+//             res.status(500).send("Error downloading file");
+//           }
+//         });
+//       } else {
+//         console.log("File is not ready or not a regular file");
+//       }
+//     }
+//   });
+    if (fs.stat(filePath).isFile()) {
+      res.download(filePath, fileName, (err) => {
+        if (err) {
+          res.status(500).send("Error downloading file");
+        }
+      });
     } else {
-      if (stats.isFile()) {
-        res.download(filePath, fileName, (err) => {
-          if (err) {
-            res.status(500).send("Error downloading file");
-          }
-        });
-      } else {
-        res.sendFile(path.join(__dirname, "staticAssets", "index.html"));
-      }
+        res.sendFile(__dirname + "/staticAssets/index.html");
     }
-  });
 });
 
 module.exports = router;
