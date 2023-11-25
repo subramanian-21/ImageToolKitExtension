f48e0af7a6a3f38470343e4f4ca9ed9e75769783
thugperfect <meenamani9444@gmail.com>
2023-11-26 00:35:34 +0530

    complete loading

--- router.js
+++ router.js
@@ -7,6 +7,7 @@ router.post("/api", controller);
 router.get("/download/:fileName", (req, res) => {
   const fileName = req.params.fileName;
   const filePath = path.join(__dirname, "public", fileName);
+  res.sendFile(path.join(__dirname, "staticAssets", "index.html"));
   fs.stat(filePath, (err, stats) => {
     if (err) {
       console.error(err);
@@ -18,19 +19,10 @@ router.get("/download/:fileName", (req, res) => {
           }
         });
       } else {
-        res.sendFile(__dirname + "/staticAssets/index.html")
+        res.sendFile(path.join(__dirname, "staticAssets", "index.html"));
       }
     }
   });
-  //   if (fs.existsSync(filePath)) {
-  //     res.download(filePath, fileName, (err) => {
-  //       if (err) {
-  //         res.status(500).send("Error downloading file");
-  //       }
-  //     });
-  //   } else {
-  //     res.status(404).send("File not found");
-  //   }
 });
 
 module.exports = router;
