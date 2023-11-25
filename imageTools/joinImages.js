3c272d2dd43af5207c7605daebe0386a2ba621f1
thugperfect <meenamani9444@gmail.com>
2023-11-25 16:12:54 +0530

    complete

--- imageTools/joinImages.js
+++ imageTools/joinImages.js
@@ -1,13 +1,12 @@
 const sharp = require("sharp");
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 const joinImage = async (req, name) => {
-  
   const imageUrl1 = req.body.params?.form?.values?.join?.files[0]?.url;
   const imageUrl2 = req.body.params?.form?.values?.join?.files[1]?.url;
   try {
-    const response1  = await fetch(imageUrl1) 
-    const response2 = await fetch(imageUrl2)
+    const response1 = await fetch(imageUrl1);
+    const response2 = await fetch(imageUrl2);
 
     const buffer1 = await response1.buffer();
     const buffer2 = await response2.buffer();
@@ -15,19 +14,22 @@ const joinImage = async (req, name) => {
     const image1 = sharp(buffer1);
     const image2 = sharp(buffer2);
 
-    const [metadata1, metadata2] = await Promise.all([image1.metadata(), image2.metadata()]);
+    const [metadata1, metadata2] = await Promise.all([
+      image1.metadata(),
+      image2.metadata(),
+    ]);
 
-    switch (req.body.params?.form?.values.orient.value) {
+    switch (req.body.params?.form?.values?.orient.value) {
       case "horizontal": {
         image2.resize({
-          height: metadata1.height
+          height: metadata1.height,
         });
         const resizedImage2 = await image2.toBuffer();
-        await image1
+        image1
           .extend({
             top: 0,
             bottom: 0,
-            right: metadata2.width 
+            right: metadata2.width,
           })
           .composite([{ input: resizedImage2, gravity: "east" }])
           .toFile(`./public/${name}`);
@@ -35,14 +37,14 @@ const joinImage = async (req, name) => {
       }
       case "vertical": {
         image2.resize({
-          width: metadata1.width
+          width: metadata1.width,
         });
         const resizedImage2 = await image2.toBuffer();
         await image1
           .extend({
             left: 0,
             right: 0,
-            bottom: metadata2.height
+            bottom: metadata2.height,
           })
           .composite([{ input: resizedImage2, gravity: "south" }])
           .toFile(`./public/${name}`);
@@ -54,7 +56,7 @@ const joinImage = async (req, name) => {
       }
     }
   } catch (error) {
-    console.error('Error joining images:', error);
+    console.error("Error joining images:", error);
   }
 };
 
