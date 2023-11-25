7378302b317c8daab5ef31394356b77075a5e3f8
thugperfect <meenamani9444@gmail.com>
2023-11-25 15:41:47 +0530

    complete join

--- imageTools/joinImages.js
+++ imageTools/joinImages.js
@@ -1,41 +1,61 @@
 const sharp = require("sharp");
+const fetch = require('node-fetch');
+
 const joinImage = async (req, name) => {
   const imageUrl1 = req.body.params?.form?.values?.join?.files[0]?.url;
   const imageUrl2 = req.body.params?.form?.values?.join?.files[1]?.url;
 
-  const response1 = await fetch(imageUrl1);
-  const response2 = await fetch(imageUrl2);
+  try {
+    const response1  = await fetch(imageUrl1) 
+    const response2 = await fetch(imageUrl2)
 
-  const buffer1 = await response1.arrayBuffer();
-  const buffer2 = await response2.arrayBuffer();
+    const buffer1 = await response1.buffer();
+    const buffer2 = await response2.buffer();
 
-  const image1 = sharp(buffer1);
-  const image2 = sharp(buffer2); 
-  const [metadata1, metadata2] = await Promise.all([image1.metadata(), image2.metadata()]);
+    const image1 = sharp(buffer1);
+    const image2 = sharp(buffer2);
 
-  switch (req.body.params?.form?.values.orient.value) {
-    case "horizontal": {
-        await image2.resize({
-            height: metadata1.height,
-            width:metadata1.width
-          });
-          const image2Buffer = await image2.toBuffer()
-          const joinedImageHorizontal = await image1
-          console.log("Joined horizontally:", joinedImageHorizontal);
-          break;
-    }
-    case "vertical": {
+    const [metadata1, metadata2] = await Promise.all([image1.metadata(), image2.metadata()]);
+
+    switch (req.body.params?.form?.values.orient.value) {
+      case "horizontal": {
         image2.resize({
-            height: metadata1.height,
-            width:metadata1.width
-          });
-          const image2Buffer = await image2.toBuffer()
-          const joinedImageVertical = await image1.composite([{ input:image2Buffer, gravity: 'south' }]).toFile(`./public/${name}`);
-          console.log("Joined vertically:", joinedImageVertical);
-          break;
+          height: metadata1.height
+        });
+        const resizedImage2 = await image2.toBuffer();
+        await image1
+          .extend({
+            top: 0,
+            bottom: 0,
+            right: metadata2.width 
+          })
+          .composite([{ input: resizedImage2, gravity: "east" }])
+          .toFile(`./public/${name}`);
+        break;
+      }
+      case "vertical": {
+        image2.resize({
+          width: metadata1.width
+        });
+        const resizedImage2 = await image2.toBuffer();
+        await image1
+          .extend({
+            left: 0,
+            right: 0,
+            bottom: metadata2.height
+          })
+          .composite([{ input: resizedImage2, gravity: "south" }])
+          .toFile(`./public/${name}`);
+        break;
+      }
+      default: {
+        console.error("Invalid orientation");
+        break;
+      }
     }
+  } catch (error) {
+    console.error('Error joining images:', error);
   }
-
-  console.log("Image saving complete");
 };
+
 module.exports = joinImage;
