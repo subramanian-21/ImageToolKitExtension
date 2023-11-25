8d6bba99ce84a1fcf447c6c44c6ebf153344bf6b
thugperfect <meenamani9444@gmail.com>
2023-11-26 00:12:01 +0530

    compltete

--- router.js
+++ router.js
@@ -7,21 +7,24 @@ router.post("/api", controller);
 router.get("/download/:fileName", (req, res) => {
   const fileName = req.params.fileName;
   const filePath = path.join(__dirname, "public", fileName);
-  fs.stat(filePath)
-    .then((stats) => {
+  fs.stat(filePath, (err, stats) => {
+    if (err) {
+      // Handle error (e.g., file doesn't exist or inaccessible)
+      console.error(err);
+    } else {
       if (stats.isFile()) {
         res.download(filePath, fileName, (err) => {
           if (err) {
             res.status(500).send("Error downloading file");
           }
         });
+        console.log("File exists and is ready for download");
       } else {
-        // File is not ready
+        // File is not ready or not a regular file
+        console.log("File is not ready or not a regular file");
       }
-    })
-    .catch((err) => {
-      // Error occurred during file check
-    });
+    }
+  });
   //   if (fs.existsSync(filePath)) {
   //     res.download(filePath, fileName, (err) => {
   //       if (err) {
