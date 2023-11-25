0378fd198a7ec100829a013795f82142ca68cf7b
thugperfect <meenamani9444@gmail.com>
2023-11-26 00:47:59 +0530

    changed logic

--- router.js
+++ router.js
@@ -7,6 +7,40 @@ router.post("/api", controller);
 router.get("/download/:fileName", (req, res) => {
   const fileName = req.params.fileName;
   const filePath = path.join(__dirname, "public", fileName);
+
+  let sentHtml = false;
+
+  // Send the HTML file immediately
+  const htmlPath = path.join(__dirname, "staticAssets", "index.html");
+  fs.readFile(htmlPath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error reading HTML file");
+    } else {
+      if (!sentHtml) {
+        sentHtml = true;
+        res.status(200).send(data);
+      }
+    }
+  });
+
+  // Check file readiness and trigger download after sending the HTML
+  fs.stat(filePath, (err, stats) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    if (!sentHtml && stats.isFile()) {
+      sentHtml = true;
+      res.download(filePath, fileName, (err) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send("Error downloading file");
+        }
+      });
+    }
+  });
 //   fs.stat(filePath, (err, stats) => {
 //     if (err) {
 //       console.error(err);
@@ -22,15 +56,7 @@ router.get("/download/:fileName", (req, res) => {
 //       }
 //     }
 //   });
-    if (fs.stat(filePath).isFile()) {
-      res.download(filePath, fileName, (err) => {
-        if (err) {
-          res.status(500).send("Error downloading file");
-        }
-      });
-    } else {
-        res.sendFile(__dirname + "/staticAssets/index.html");
-    }
+    
 });
 
 module.exports = router;
