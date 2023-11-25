5202c84a34b3c5c903d44c654d5afae7bbd0d2f9
thugperfect <meenamani9444@gmail.com>
2023-11-26 00:50:04 +0530

    revert changes

--- router.js
+++ router.js
@@ -8,54 +8,21 @@ router.get("/download/:fileName", (req, res) => {
   const fileName = req.params.fileName;
   const filePath = path.join(__dirname, "public", fileName);
 
-  let sentHtml = false;
-
-  // Send the HTML file immediately
-  const htmlPath = path.join(__dirname, "staticAssets", "index.html");
-  fs.readFile(htmlPath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Error reading HTML file");
-    } else {
-      if (!sentHtml) {
-        sentHtml = true;
-        res.status(200).send(data);
-      }
-    }
-  });
-
-  // Check file readiness and trigger download after sending the HTML
   fs.stat(filePath, (err, stats) => {
     if (err) {
       console.error(err);
-      return;
-    }
-
-    if (!sentHtml && stats.isFile()) {
-      sentHtml = true;
-      res.download(filePath, fileName, (err) => {
-        if (err) {
-          console.error(err);
-          res.status(500).send("Error downloading file");
-        }
-      });
+    } else {
+      if (stats.isFile()) {
+        res.download(filePath, fileName, (err) => {
+          if (err) {
+            res.status(500).send("Error downloading file");
+          }
+        });
+      } else {
+        res.write("hello")
+      }
     }
   });
-//   fs.stat(filePath, (err, stats) => {
-//     if (err) {
-//       console.error(err);
-//     } else {
-//       if (stats.isFile()) {
-//         res.download(filePath, fileName, (err) => {
-//           if (err) {
-//             res.status(500).send("Error downloading file");
-//           }
-//         });
-//       } else {
-//         console.log("File is not ready or not a regular file");
-//       }
-//     }
-//   });
     
 });
 
