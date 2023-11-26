606ff798ca6731d9a16653736801cd22fc68191f
thugperfect <meenamani9444@gmail.com>
2023-11-27 00:52:37 +0530

    download read

--- router.js
+++ router.js
@@ -8,22 +8,30 @@ router.post("/api", controller);
 router.get("/download/:fileName", (req, res) => {
   const fileName = req.params.fileName;
   const filePath = path.join(__dirname, "public", fileName);
-
-  fs.stat(filePath, (err, stats) => {
-    if (err) {
-      console.error(err);
-    } else {
-      if (stats.isFile()) {
-        res.download(filePath, fileName, (err) => {
-          if (err) {
-            res.status(500).send("Error downloading file");
-          }
-        });
-      } else {
-        console.log("Error...")
-      }
-    }
+  const fileStream = fs.createReadStream(filePath);
+  fileStream.on('open', () => {
+    res.set('Content-Disposition', `attachment; filename="${fileName}"`);
+    fileStream.pipe(res);
   });
+  fileStream.on('error', (err) => {
+    console.error(err);
+    res.status(500).end('Internal Server Error');
+  });
+  // fs.stat(filePath, (err, stats) => {
+  //   if (err) {
+  //     console.error(err);
+  //   } else {
+  //     if (stats.isFile()) {
+  //       res.download(filePath, fileName, (err) => {
+  //         if (err) {
+  //           console.log(err)
+  //         }
+  //       });
+  //     } else {
+  //       console.log("Error...")
+  //     }
+  //   }
+  // });
 });
 
 module.exports = router;
