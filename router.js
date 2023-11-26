c2f877ccd1a61f822761fd71d5e5f36e902225cd
thugperfect <meenamani9444@gmail.com>
2023-11-27 01:14:42 +0530

    changed

--- router.js
+++ router.js
@@ -4,18 +4,21 @@ const path = require("path");
 
 const fs = require("fs");
 router.post("/api", controller);
-
 router.get("/download/:fileName", (req, res) => {
   const fileName = req.params.fileName;
   const filePath = path.join(__dirname, "public", fileName);
-  const fileStream = fs.createReadStream(filePath);
-  fileStream.on('open', () => {
-    res.set('Content-Disposition', `attachment; filename="${fileName}"`);
-    fileStream.pipe(res);
-  });
-  fileStream.on('error', (err) => {
-    console.error(err);
-    
+
+  fs.stat(filePath, (err, stats) => {
+    if (err) {
+      console.error(err);
+    } else {
+      if (stats.isFile()) {
+        res.set('Content-Disposition', `attachment; filename="${fileName}"`);
+        
+      } else {
+        console.log("Error...")
+      }
+    }
   });
 });
 
