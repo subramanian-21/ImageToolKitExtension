3e3ce7343b91155bcf43cfe62888f495aac3655c
thugperfect <meenamani9444@gmail.com>
2023-11-27 11:51:52 +0530

    try catch for down;oad

--- router.js
+++ router.js
@@ -7,20 +7,25 @@ router.post("/api", controller);
 router.get("/download/:fileName", (req, res) => {
   const fileName = req.params.fileName;
   const filePath = path.join(__dirname, "public", fileName);
-
-   fs.stat(filePath, (err, stats) => {
-    if (err) {
-      console.error(err);
-    } else {
-      if (stats.isFile()) {
-        res.download(filePath, fileName, (err) => {
-          if (err) {console.log(err)         }
-        });
+  try {
+    fs.stat(filePath, (err, stats) => {
+      if (err) {
+        console.error(err);
       } else {
-        console.log("Error...");
+        if (stats.isFile()) {
+          res.download(filePath, fileName, (err) => {
+            if (err) {console.log(err)         }
+          });
+        } else {
+          console.log("Error...");
+        }
       }
-    }
-  });
+    });
+    
+  } catch (error) {
+    console.log(err)
+  }
+   
 });
 
 module.exports = router;
