f0e30a35aec1536bc501426d958fddb2461b709f
thugperfect <meenamani9444@gmail.com>
2023-11-27 12:28:56 +0530

    fs stat in set timeout

--- router.js
+++ router.js
@@ -8,19 +8,22 @@ router.get("/download/:fileName", (req, res) => {
   const fileName = req.params.fileName;
   const filePath = path.join(__dirname, "public", fileName);
   try {
-    fs.stat(filePath, (err, stats) => {
-      if (err) {
-        console.error(err);
-      } else {
-        if (stats.isFile()) {
-          res.download(filePath, fileName, (err) => {
-            if (err) {console.log(err)         }
-          });
+    setTimeout(()=>{
+      fs.stat(filePath, (err, stats) => {
+        if (err) {
+          console.error(err);
         } else {
-          console.log("Error...");
+          if (stats.isFile()) {
+            res.download(filePath, fileName, (err) => {
+              if (err) {console.log(err)         }
+            });
+          } else {
+            console.log("Error...");
+          }
         }
-      }
-    });
+      });
+      
+    },2000)
     
   } catch (error) {
     console.log(err)
