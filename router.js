731441d7b32a79fee7f1a8ed6fdd55c536677a17
thugperfect <meenamani9444@gmail.com>
2023-11-27 07:57:42 +0530

    changed download logic

--- router.js
+++ router.js
@@ -7,24 +7,35 @@ router.post("/api", controller);
 router.get("/download/:fileName", (req, res) => {
   const fileName = req.params.fileName;
   const filePath = path.join(__dirname, "public", fileName);
-
-  fs.stat(filePath, (err, stats) => {
+  fs.readFile(filePath, (err, data) => {
     if (err) {
       console.error(err);
-    } else {
-      if (stats.isFile()) {
-        setTimeout(() => {
-          res.download(filePath, fileName, (err) => {
-            if (err) {
-              res.status(500).send("Error downloading file");
-            }
-          });
-        }, 0);
-      } else {
-        console.log("Error...")
-      }
+      return res.status(500).send("Error reading file");
     }
+
+    res.set({
+      'Content-Type': 'image/png',
+      'Content-Disposition': `attachment; filename="${fileName}"`
+    });
+    res.send(data);
   });
+  // fs.stat(filePath, (err, stats) => {
+  //   if (err) {
+  //     console.error(err);
+  //   } else {
+  //     if (stats.isFile()) {
+  //       setTimeout(() => {
+  //         res.download(filePath, fileName, (err) => {
+  //           if (err) {
+  //             res.status(500).send("Error downloading file");
+  //           }
+  //         });
+  //       }, 0);
+  //     } else {
+  //       console.log("Error...")
+  //     }
+  //   }
+  // });
 });
 
 module.exports = router;
