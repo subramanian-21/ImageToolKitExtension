d07b7edf416082f962d28dd61f55394ff8fe1ee7
thugperfect <meenamani9444@gmail.com>
2023-12-03 01:03:12 +0530

    complete

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.use((req, res) => {
      res.sendFile(__dirname + "/staticAssets/index.html");
   }
 });
-app.listen(5001, "0.0.0.0",() => {
+app.listen(3000, "0.0.0.0",() => {
   console.log("Server@5001");
 });
